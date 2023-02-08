ec2ed7452d566c5a456424beadb8f1df1c22a4ba
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-07 21:10:41 -0700

    inital commit with all files and DB created

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import json
+from dbhelpers import run_statement
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+app.run(debug=True)
---
+++ dbhelpers.py
@@ -0,0 +1,60 @@
+import mariadb
+import dbcreds
+
+
+def connect_db():
+    try:
+        conn = mariadb.connect(
+            user = dbcreds.user,
+            password = dbcreds.password,
+            host = dbcreds.host,
+            port = dbcreds.port,
+            database = dbcreds.database,
+            autocommit = True
+            )
+
+        cursor = conn.cursor()
+        return cursor
+    except mariadb.OperationalError as e:
+        print("Operational Error:", e)
+    except Exception as e:
+        print("Unexpected Error:",e)
+
+def disconnect_db(cursor):
+    try:
+        conn = cursor.connection
+        cursor.close()
+        conn.close()
+    except mariadb.OperationalError as e:
+        print("Operational Error:", e)
+    except mariadb.InternalError as e:
+        print("Internal Error:",e)
+    except Exception as e:
+        print("Unexpected Error:",e)
+
+def execute_statement(cursor,statement,args={}):
+    try:
+        cursor.excute(statement, args)
+        results = cursor.fetchall()
+        return results
+    except mariadb.ProgrammingError as e:
+        print("Syntax error in your SQL statement:", e)
+        return str(e)
+    except mariadb.IntegrityError as e:
+        print("the statement failed to execute due to intergity error,", e)
+        return str(e)
+    except mariadb.DataError as e:
+        print("data error:",e)
+        return str(e)
+    except Exception as e:
+        print("Unexpected Error:",e)
+        return str(e)
+
+def run_statement(statement, args=[]):
+    cursor = connect_db()
+    if (cursor == None):
+        print("Failed to connect to DB, statement will not run")
+        return  None
+    result = execute_statement(cursor, statement, args)
+    disconnect_db(cursor)
+    return result
