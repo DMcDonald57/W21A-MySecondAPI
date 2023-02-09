046af6b6f3db4d2f531df3b1ef807fd0c824da07
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-08 20:30:40 -0700

    All functions work except for the success messages.

--- app.py
+++ app.py
@@ -46,9 +46,50 @@ def deleteItem():
     else:
         return "There was an error"
 
+# The employee functions
 
+@app.get('/api/employees')
+def employeeinfo():
+    id = request.json.get('id')
+    result = run_statement('CALL employeeinfo (?)',[id])
+    if (type(result) == list):
+        return json.dumps(result, default=str)
+    else:
+        return "There was an error"
 
+# # This post works as it added employee to DB but return message does not.  I still get error msg.
 
+@app.post('/api/employees')
+def newEmployee():
+    name = request.json.get('name')
+    hourly_wage = request.json.get('hourly_wage')
+    result = run_statement('CALL newEmployee (?,?)',[name, hourly_wage])
+    for name in result:
+        return "Welcome {}.".format(name[0])
+    else:
+        return "There was an error"
+
+# # This patch works as it updated the quantity in the DB but return message does not.  I still get error msg.
+@app.patch('/api/employees')
+def updateEmployee():
+    id = request.json.get('id')
+    hourly_wage = request.json.get('hourly_wage')
+    result = run_statement('CALL updateEmployee (?,?)',[id,hourly_wage])
+    for hourly_wage in result:
+        return "New hourly wage: {}".format(hourly_wage)
+    else:
+        return "There was an error"
+
+# # This delete works as it deleted the item in the DB but return message does not.  I still get error msg.
+
+@app.delete('/api/employees')
+def deleteEmployee():
+    id = request.json.get('id')
+    result = run_statement('CALL deleteEmployee (?)',[id])
+    if result == None:
+        return "Employee deleted"
+    else:
+        return "There was an error"
 
 
 app.run(debug=True)
